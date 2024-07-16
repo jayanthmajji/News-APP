864781797ef4320c8449e1eb840cd8b30245a0f4
jayanthmajji <jayanthmajji45@gmail.com>
2024-07-16 21:52:49 +0530

    created a news app

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-const API_KEY = "12bee16f423748c08430c8e0ff0627ae";
-const url = "https://newsapi.org/v2/everything?q=";
+const API_KEY = "pub_48723489487b6c9865539e2abb2aab44cb873";
+const BASE_URL = "https://newsdata.io/api/1/latest";
 
 window.addEventListener("load", () => fetchNews("India"));
 
@@ -9,9 +9,12 @@ function reload() {
 }
 
 async function fetchNews(query) {
-    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const url = `${BASE_URL}?apiKey=${API_KEY}&q=${query}`;
+    console.log("url ",url);
+    const res = await fetch(url);
+    console.log("result---",res);
     const data = await res.json();
-    bindData(data.articles);
+    bindData(data.results);
 }
 
 function bindData(articles) {
@@ -21,35 +24,35 @@ function bindData(articles) {
     cardsContainer.innerHTML = "";
 
     articles.forEach((article) => {
-        if (!article.urlToImage) return;
+        if (!article.image_url) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article)
         cardsContainer.appendChild(cardClone);
     });
 }
 
-
 function fillDataInCard(cardClone, article){
+  console.log();
   const newsImg = cardClone.querySelector("#news-img")
   const newsTitle = cardClone.querySelector("#news-title")
   const newsSource = cardClone.querySelector("#news-source")
   const newsDesc = cardClone.querySelector("#news-desc")
- 
-  newsImg.src = article.urlToImage;
+  let dec = String(article.description);
+  newsImg.src = article.image_url;
   newsTitle.innerHTML = article.title;
-  newsDesc.innerHTML = article.description;
+  newsDesc.innerHTML = dec.substring(0,100);
 
-  const date = new Date(article.publishedAt).toLocaleString("en-US",{
+  const date = new Date(article.pubDate).toLocaleString("en-US",{
     timeZone: "Asia/Jakarta",
   })
 
-  newsSource.innerHTML = `${article.source.name} . ${date}`
+  newsSource.innerHTML = `<a>${article.source_url}</a>`
 
   cardClone.firstElementChild.addEventListener("click", () =>{
-    window.open(article.url, "_blank")
+    window.open(article.link, "_blank")
   })
 };
-
+// console.log(newsImg);
 let curSelectedNav = null;
 
 function onNavItemClick(id){
